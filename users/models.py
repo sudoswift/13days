3852d9720e8796de6cda08517b22e71a7c26e306
sudoswift <sudoswift@gmail.com>
2022-05-08 23:26:08 +0900

    complete users models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
+from locale import currency
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    
+    """Custom User Mdoel"""
+    
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    
+    GENDER_CHOICES = [
+        (GENDER_MALE, 'MALE'),
+        (GENDER_FEMALE, 'FEMALE'),
+        (GENDER_OTHER,'OTHER'),
+    ]
+    
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    
+    LANGUAGE_CHOICES = [
+        (LANGUAGE_ENGLISH, 'EN'),
+        (LANGUAGE_KOREAN, 'KR'),
+    ]
+    
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    
+    CURRENCY_CHOICES = [
+        (CURRENCY_USD, 'USD'),
+        (CURRENCY_KRW, 'KRW'),
+    ]
+    
+    avatar = models.ImageField(null=True, blank = True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank = True)
+    bio = models.TextField(default="", blank = True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(max_length=3, choices=LANGUAGE_CHOICES, null=True, blank = True)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, null=True, blank = True)
